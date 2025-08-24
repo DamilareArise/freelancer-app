525d4524d8f556a70edfd7d66d7bd9f16819b14a
Arise Damilare <arisedamilare5@gmail.com>
2025-08-24 18:57:48 +0100

    removed collect static

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "Applying database migrations..."
 python freelancer/manage.py migrate --noinput
 
-echo "Collecting static files..."
-python freelancer/manage.py collectstatic --noinput
-
 echo "Starting Gunicorn..."
 exec gunicorn freelancer.wsgi:application --chdir freelancer --bind 0.0.0.0:8000
