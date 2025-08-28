e25b70fdca5019b0eb3f9182ec32ccd4254bba53
Arise Damilare <arisedamilare5@gmail.com>
2025-08-28 12:45:31 +0100

    Refactor entrypoint.sh to simplify command handling and ensure correct path for manage.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,18 @@
 #!/bin/sh
 set -e
 
-CMD="$1"
-
-if [ -z "$CMD" ]; then
-  CMD="web"   # default to web if nothing passed
-fi
-
-case "$CMD" in
+case "$1" in
   web)
     echo "Starting Gunicorn (Django web)..."
-    python freelancer/manage.py migrate --noinput
-    python freelancer/manage.py role_seed
+    python manage.py migrate --noinput
     exec gunicorn freelancer.wsgi:application --bind 0.0.0.0:8000 --workers 3
     ;;
-    
   celery)
     echo "Starting Celery worker..."
     exec celery -A freelancer worker -l info
     ;;
-
-  # celery-beat)
-  #   echo "Starting Celery Beat..."
-  #   exec celery -A freelancer beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
-  #   ;;
-
   *)
-    echo "Unknown command: $CMD"
+    echo "Unknown command: $1"
     echo "Usage: entrypoint.sh {web|celery|celery-beat}"
     exit 1
     ;;
