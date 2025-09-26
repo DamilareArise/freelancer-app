32d22577086f34403e2754786c58cb4fe3e0d6a3
Arise Damilare <arisedamilare5@gmail.com>
2025-09-26 17:08:34 +0100

    updated email setup

--- freelancer/freelancer/settings.py
+++ freelancer/freelancer/settings.py
@@ -168,6 +168,8 @@ EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=True)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=False)
 
 # Celery & Redis
 CELERY_BROKER_URL = config("CELERY_BROKER_URL")
