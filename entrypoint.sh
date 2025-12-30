ce599e07695ac91cacda580b342bbe4592981ed8
Arise Damilare <arisedamilare5@gmail.com>
2025-12-30 12:37:14 +0100

    Refactor: seed command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ case "$1" in
   web)
     echo "Starting Gunicorn (Django web)..."
     python manage.py migrate --noinput
-    python manage.py availability.py
+    python manage.py availability
     exec gunicorn freelancer.wsgi:application --bind 0.0.0.0:8000 --workers 3
     ;;
   celery)
